0ea16023cd11e0b8a670d13bf22111010ab8b1c4
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2019-12-07 01:05:20 +0100

    Working OBJ loader
    Implemented a working OBJ loader.

--- 16SimpleObjectLoader/src/ImportedModel.java
+++ 16SimpleObjectLoader/src/ImportedModel.java
@@ -13,15 +13,11 @@ public class ImportedModel {
 			
 			numVertices = modelImporter.getNumVertices();
 			float[] verts = modelImporter.getVertices();
-			float[] tcs = modelImporter.getTextureCoordinates();
-			float[] normals = modelImporter.getNormals();
 			
 			vertices = new Vertex3D[numVertices];
 			for (int i = 0; i < vertices.length; i++) {
 				vertices[i] = new Vertex3D();
 				vertices[i].setLocation(verts[i*3], verts[i*3+1], verts[i*3+2]);
-				vertices[i].setST(tcs[i*2], tcs[i*2+1]);
-				vertices[i].setNormal(normals[i*3], normals[i*3+1], normals[i*3+2]);
 			}
 			
 		} catch (Exception e) {
